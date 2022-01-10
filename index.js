563afa76ead3e698a290bb269ad81b8ed3b50973
Yuke Zhao <yuke-zhao@outlook.com>
2022-01-10 14:38:53 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+var main = document.getElementById("main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.innerHTML = "Yuke is the champion"
